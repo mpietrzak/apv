1d6f10d4c003dfa3c929a4209fd711bf26b9ad63
maciej <maciej@localhost>
2009-12-25 23:49:52 +0100

    project settings, Application.mk and top level jni/Android.mk

--- pdfview/ndk-app/Application.mk
+++ pdfview/ndk-app/Application.mk
@@ -1,2 +1,2 @@
 APP_MODULES := png mupdf fitz fitzdraw jpeg pdfview2 
-APP_PROJECT_PATH := /cygdrive/c/fun/android/eclipse-workspace/pdfview2
+APP_PROJECT_PATH := /cygdrive/c/fun/android/eclipse-workspace-hg/pdfview/pdfview
