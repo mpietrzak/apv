37349d2697f6bd1be596d489ea276c6b55c376d3
maciej <maciej@hell.cx>
2011-04-16 22:04:45 +0200

    use android ndk to build freetype, update mupdf

--- pdfview/scripts/android-gcc-setup.sh
+++ pdfview/scripts/android-gcc-setup.sh
@@ -3,13 +3,13 @@
 # . ./android-gcc-setup.sh
 # to setup build environment
 
-export NDK=/cygdrive/d/Code/android/android-ndk-r3
-HOST_TYPE=windows
+export NDK=$HOME/fun/android/android-ndk
+HOST_TYPE=darwin-x86
 ANDROID_VERSION=3
 LIBDIR=$NDK/build/platforms/android-$ANDROID_VERSION/arch-arm/usr/lib
 
 export CFLAGS="-march=armv5te -mtune=xscale"
-export CC=$NDK/build/prebuilt/$HOST_TYPE/arm-eabi-4.2.1/bin/arm-eabi-gcc
+export CC=$NDK/toolchains/arm-eabi-4.4.0/prebuilt/$HOST_TYPE/bin/arm-eabi-gcc
 export CPPFLAGS=-I$NDK/build/platforms/android-$ANDROID_VERSION/arch-arm/usr/include
 export LDFLAGS="-L$LIBDIR -nostdlib -ldl -lc -lz $LIBDIR/crtbegin_static.o"
 
