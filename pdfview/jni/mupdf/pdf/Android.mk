676c86dc785157afaa1d15f6d8498a4599e26f92
maciej <maciej@hell.cx>
2011-04-16 22:04:45 +0200

    use android ndk to build freetype, update mupdf

--- pdfview/jni/mupdf/pdf/Android.mk
+++ pdfview/jni/mupdf/pdf/Android.mk
@@ -2,11 +2,10 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../fitz $(LOCAL_PATH)/../../pdfview2/include
-LOCAL_MODULE    := mupdf
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../fitz $(LOCAL_PATH)/../../freetype-overtlay/include $(LOCAL_PATH)/../../freetype/include $(LOCAL_PATH)/..
+LOCAL_MODULE    := pdf
 LOCAL_CFLAGS := -DNOCJK
 LOCAL_SRC_FILES := \
-	pdf_crypt.c \
 	apv_pdf_debug.c \
 	pdf_lex.c \
 	pdf_nametree.c \
@@ -20,12 +19,11 @@ LOCAL_SRC_FILES := \
 	pdf_cmap_parse.c \
 	pdf_cmap_load.c \
 	pdf_cmap_table.c \
-	pdf_fontagl.c \
-	pdf_fontenc.c \
+	pdf_encoding.c \
 	pdf_unicode.c \
 	pdf_font.c \
 	pdf_type3.c \
-	pdf_fontmtx.c \
+	pdf_metrics.c \
 	pdf_fontfile.c \
 	pdf_function.c \
 	pdf_colorspace.c \
@@ -33,16 +31,10 @@ LOCAL_SRC_FILES := \
 	pdf_pattern.c \
 	pdf_shade.c \
 	pdf_xobject.c \
-	pdf_build.c \
 	pdf_interpret.c \
 	pdf_page.c \
-	pdf_pagetree.c \
 	pdf_store.c \
-	\
- 	font_misc.c \
- 	font_mono.c \
- 	font_sans.c \
- 	font_serif.c \
+	pdf_crypt.c
 
 #	cmap_tounicode.c \
 # 	font_cjk.c \
