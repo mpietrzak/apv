0904ed2de69d3130dfcd8bb0bbcf4359a527c71c
Alexander Pruss <arpruss@mgmail.com>
2011-07-19 08:21:10 -0700

    Varia

--- pdfview/src/cx/hell/android/pdfview/ChooseFileActivity.java
+++ pdfview/src/cx/hell/android/pdfview/ChooseFileActivity.java
@@ -8,6 +8,7 @@ import java.util.Comparator;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -30,20 +31,28 @@ public class ChooseFileActivity extends Activity implements OnItemClickListener 
 	 * Logging tag.
 	 */
 	private final static String TAG = "cx.hell.android.pdfview";
+	private final static String PREF_TAG = "ChooseFileActivity";
+	private final static String PREF_HOME = "Home";
+	private final static int HOME_POSITION = 0;
+	private final static int RECENT_START = 1;
 	
-	private String currentPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+	private String currentPath;
 	
 	private TextView pathTextView = null;
 	private ListView filesListView = null;
 	private FileFilter fileFilter = null;
 	private ArrayAdapter<String> fileListAdapter = null;
+	private Recent recent = null;
 	
 	private MenuItem aboutMenuItem = null;
+	private MenuItem setAsHomeMenuItem = null;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
     	super.onCreate(savedInstanceState);
     	
+    	currentPath = getHome(); 
+
     	this.fileFilter = new FileFilter() {
     		public boolean accept(File f) {
     			return (f.isDirectory() || f.getName().toLowerCase().endsWith(".pdf"));
@@ -57,7 +66,6 @@ public class ChooseFileActivity extends Activity implements OnItemClickListener 
     	this.fileListAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
     	this.filesListView.setAdapter(this.fileListAdapter);
     	this.filesListView.setOnItemClickListener(this);
-    	this.update();
     }
     
     /**
@@ -66,7 +74,9 @@ public class ChooseFileActivity extends Activity implements OnItemClickListener 
     private void update() {
     	this.pathTextView.setText(this.currentPath);
     	this.fileListAdapter.clear();
-    	this.fileListAdapter.add("..");
+    	this.fileListAdapter.add("["+getResources().getString(R.string.go_home)+"]");
+    	if (!this.currentPath.equals("/"))
+    		this.fileListAdapter.add("..");
     	
     	File files[] = new File(this.currentPath).listFiles(this.fileFilter);
     	if (files != null) {
@@ -89,29 +99,87 @@ public class ChooseFileActivity extends Activity implements OnItemClickListener 
 	    	for(int i = 0; i < files.length; ++i) this.fileListAdapter.add(files[i].getName());
     	}
     	
+    	if (isHome(currentPath)) {
+    		recent = new Recent(this);
+    		
+        	for (int i = 0; i < recent.size(); i++) {
+        		this.fileListAdapter.insert(""+(i+1)+": "+(new File(recent.get(i))).getName(), 
+        				RECENT_START+i);
+        	}
+    	}
+    	else {
+    		recent = null;
+    	}
+    	
     	this.filesListView.setSelection(0);
+    }
+    
+    public void pdfView(File f) {
+		Log.i(TAG, "post intent to open file " + f);
+		Intent intent = new Intent();
+		intent.setDataAndType(Uri.fromFile(f), "application/pdf");
+		intent.setClass(this, OpenFileActivity.class);
+		intent.setAction("android.intent.action.VIEW");
+		this.startActivity(intent);
+    }
+    
+    private boolean isHome(String path) {
+    	File pathFile = new File(path);
+    	File homeFile = new File(getHome());
+    	try {
+			return pathFile.getCanonicalPath().equals(homeFile.getCanonicalPath());
+		} catch (IOException e) {
+			return false;
+		}
+    }
+    
+    private String getHome() {
+    	String defaultHome = Environment.getExternalStorageDirectory().getAbsolutePath(); 
+		String path = getSharedPreferences(PREF_TAG, 0).getString(PREF_HOME,
+							defaultHome);
+		if (path.length()>1 && path.endsWith("/")) {
+			path = path.substring(0,path.length()-2);
+		}
+
+		File pathFile = new File(path);
+
+		if (pathFile.exists() && pathFile.isDirectory())
+			return path;
+		else
+			return defaultHome;
     }
     
     @SuppressWarnings("rawtypes")
 	public void onItemClick(AdapterView parent, View v, int position, long id) {
-    	String filename = (String) this.filesListView.getItemAtPosition(position);
-    	File clickedFile = null;
-    	try {
-    		clickedFile = new File(this.currentPath, filename).getCanonicalFile();
-    	} catch (IOException e) {
-    		throw new RuntimeException(e);
+    	File clickedFile;
+    	
+    	if (position == HOME_POSITION) {
+    		clickedFile = new File(getHome());
     	}
+    	else if (recent != null && position < RECENT_START + recent.size()) {
+    		clickedFile = new File(recent.get(position-RECENT_START));
+    	}
+    	else {
+    		String filename = (String) this.filesListView.getItemAtPosition(position);
+    		clickedFile = null;
+    		
+    		if (filename.equals("..")) {
+    			clickedFile = (new File(this.currentPath)).getParentFile();
+    		}
+    		else {
+    			clickedFile = new File(this.currentPath, filename);
+    		}
+    	}
+    	
+    	if (!clickedFile.exists())
+    		return;
+    	
     	if (clickedFile.isDirectory()) {
     		Log.d(TAG, "change dir to " + clickedFile);
     		this.currentPath = clickedFile.getAbsolutePath();
     		this.update();
     	} else {
-    		Log.i(TAG, "post intent to open file " + clickedFile);
-    		Intent intent = new Intent();
-    		intent.setDataAndType(Uri.fromFile(clickedFile), "application/pdf");
-    		intent.setClass(this, OpenFileActivity.class);
-    		intent.setAction("android.intent.action.VIEW");
-    		this.startActivity(intent);
+    		pdfView(clickedFile);
     	}
     }
 
@@ -123,12 +191,25 @@ public class ChooseFileActivity extends Activity implements OnItemClickListener 
 			this.startActivity(intent);
     		return true;
     	}
+    	else if (menuItem == this.setAsHomeMenuItem) {
+    		SharedPreferences.Editor edit = getSharedPreferences(PREF_TAG, 0).edit();
+    		edit.putString(PREF_HOME, currentPath);
+    		edit.commit();
+    		return true;
+    	}
     	return false;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+    	this.setAsHomeMenuItem = menu.add(R.string.set_as_home);
     	this.aboutMenuItem = menu.add("About");
     	return true;
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	this.update();
     }
 }
