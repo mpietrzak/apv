948c6614ebd63fc923b0cb74a8f8c88c2c9f95ea
Alexander Pruss <arpruss@gmail.com>
2011-07-20 22:26:40 -0700

    Optionally remove .pdf from list, add up-folder icon

--- pdfview/scale-images.sh
+++ pdfview/scale-images.sh
@@ -1,4 +1,4 @@
-for x in folder home recent1 recent2 recent3 recent4 recent5 ; do
+for x in upfolder folder home recent1 recent2 recent3 recent4 recent5 ; do
   convert res/drawable-hdpi/$x.png -resize 66.7% res/drawable-mdpi/$x.png
   convert res/drawable-hdpi/$x.png -resize 50% res/drawable-ldpi/$x.png
 done
