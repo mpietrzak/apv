4c8ea0430edb5c8954bab9e2cc2250278385070e
Alexander Pruss <arpruss@gmail.com>
2011-11-22 10:08:25 -0600

    import ARM-optimized mupdf snapshot

--- pdfview/jni/mupdf/draw/Android.mk
+++ pdfview/jni/mupdf/draw/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS := -O3
+LOCAL_CFLAGS := -O3 -DARM_ARCH
 LOCAL_ARM_MODE := arm
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../mupdf $(LOCAL_PATH)/../fitz
