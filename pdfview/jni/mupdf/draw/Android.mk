9b4ab5266c1b90375fa40e0a6e0fb8eeced66b1e
Maciej Pietrzak <mpietrzak@gmail.com>
2012-10-06 19:58:28 +0200

    add x86 arch to default mk files

--- pdfview/jni/mupdf/draw/Android.mk
+++ pdfview/jni/mupdf/draw/Android.mk
@@ -2,8 +2,18 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_CFLAGS := -O3 -DARCH_ARM
-LOCAL_ARM_MODE := arm
+LOCAL_CFLAGS := -O3
+
+ifeq ($(TARGET_ARCH_ABI),armeabi)
+	LOCAL_CFLAGS += -DARCH_ARM 
+	LOCAL_ARM_MODE := arm
+endif
+
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+	LOCAL_CFLAGS += -DARCH_ARM 
+	LOCAL_ARM_MODE := arm
+endif
+
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../mupdf $(LOCAL_PATH)/../fitz
 LOCAL_MODULE    := fitzdraw
