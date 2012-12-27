1a7d32ea33de5da5e5173861352a76d2ecf2c1e1
Maciej Pietrzak <mpietrzak@gmail.com>
2012-12-27 03:09:48 +0100

    split pdfview2.c into android-related apvandroid.[ch] and generic apvcore.[ch]

--- pdfview/jni/pdfview2/apvandroid.h
+++ pdfview/jni/pdfview2/apvandroid.h
@@ -16,42 +16,16 @@
 #define MIN(x,y) ((x) < (y) ? (x) : (y))
 #define MAX(x,y) ((x) > (y) ? (x) : (y))
 
-/**
- * Holds pdf info.
- */
-typedef struct {
-    int last_pageno;
-    fz_context *ctx;
-    fz_document *doc;
-    int fileno; /* used only when opening by file descriptor */
-    int invalid_password;
-    char box[MAX_BOX_NAME + 1];
-} pdf_t;
 
-
-/*
- * Declarations
- */
-
-
-pdf_t* create_pdf_t();
-void free_pdf_t(pdf_t *pdf);
-pdf_t* parse_pdf_file(const char *filename, int fileno, const char* password);
 pdf_t* get_pdf_from_this(JNIEnv *env, jobject this);
 void get_size(JNIEnv *env, jobject size, int *width, int *height);
 void save_size(JNIEnv *env, jobject size, int width, int height);
-void fix_samples(unsigned char *bytes, unsigned int w, unsigned int h);
-void rgb_to_alpha(unsigned char *bytes, unsigned int w, unsigned int h);
-int get_page_size(pdf_t *pdf, int pageno, int *width, int *height);
 void pdf_android_loghandler(const char *m);
 jobject create_find_result(JNIEnv *env);
 void set_find_result_page(JNIEnv *env, jobject findResult, int page);
 void add_find_result_marker(JNIEnv *env, jobject findResult, int x0, int y0, int x1, int y1);
 void add_find_result_to_list(JNIEnv *env, jobject *list, jobject find_result);
-int convert_point_pdf_to_apv(pdf_t *pdf, int page, int *x, int *y);
-int convert_box_pdf_to_apv(pdf_t *pdf, int page, int rotation, fz_rect *bbox);
 int find_next(JNIEnv *env, jobject this, int direction);
-pdf_page* get_page(pdf_t *pdf, int pageno);
 
 
 // #ifdef pro
