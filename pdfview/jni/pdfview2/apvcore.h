bb4e93d795f5aad125b795861e36e6de57edb597
Maciej Pietrzak <mpietrzak@gmail.com>
2013-04-01 23:47:47 +0200

    cleanup jni code

--- pdfview/jni/pdfview2/apvcore.h
+++ pdfview/jni/pdfview2/apvcore.h
@@ -97,11 +97,3 @@ fz_pixmap *get_page_image_bitmap(
       int width,
       int height);
 
-
-// #ifdef pro
-// jobject create_outline_recursive(JNIEnv *env, jclass outline_class, const fz_outline *outline);
-// char* extract_text(pdf_t *pdf, int pageno);
-// #endif
-
-
-
