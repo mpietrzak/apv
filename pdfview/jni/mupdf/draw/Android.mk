cff7f8e0749ea6d850f1754207adb209895adb80
Maciej Pietrzak <mpietrzak@gmail.com>
2013-05-25 22:00:47 +0200

    upgrade mupdf, use patches instead of customized .c and .h files

--- pdfview/jni/mupdf/draw/Android.mk
+++ pdfview/jni/mupdf/draw/Android.mk
@@ -19,14 +19,14 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH)/../mupdf $(LOCAL_PATH)/../fitz
 LOCAL_MODULE    := fitzdraw
 LOCAL_SRC_FILES := \
 	draw_device.c \
-        draw_blend.c \
-        draw_glyph.c \
-        draw_affine.c \
-        draw_scale.c \
-        draw_unpack.c \
-        draw_mesh.c \
+	draw_blend.c \
+	draw_glyph.c \
+	draw_affine.c \
+	draw_scale.c \
+	draw_unpack.c \
+	draw_mesh.c \
 	draw_path.c \
-        draw_paint.c \
+	draw_paint.c \
 	draw_edge.c
 
 include $(BUILD_STATIC_LIBRARY)
